6022deea4c429bd077585114b7e602153ad4e783
juliasut <juliasutygina@gmail.com>
2021-07-01 03:27:42 -0600

    Use map() method or array to convert into numbers

--- timer1.js
+++ timer1.js
@@ -2,10 +2,12 @@
 
 const args = process.args.slice(2);
 
+args = args.map( x => Number(x));
+
 const simpleTimer = function (args) {
 
   for (let arg of args) {
-    if (!NaN(Number(arg)) && (Number(arg)) > 0) {
+    if (!NaN(arg) && (arg) > 0) {
       setTimeout(() => {
         process.stdout.write(`timer's up!`)
       }, arg * 1000)
